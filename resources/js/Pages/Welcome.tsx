52cde84df5ab24a5a7173295381fee1165a080e0
Ievhen20 <Yevhen7856@ukr.net>
2025-01-16 19:07:38 +0900

    Add Slider in Landing page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,19 +1,59 @@
-import { PageProps } from '@/types'; 
+import { PageProps } from '@/types';
 import { Head, Link } from '@inertiajs/react';
 import GuestNav from '@/Components/GuestNav';
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'; // For next and prev icons
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';  // Import datepicker styles
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Welcome({
   auth,
   laravelVersion,
   phpVersion,
 }: PageProps<{ laravelVersion: string; phpVersion: string }>) {
-
   const [startDate, setStartDate] = useState<Date | null>(null); // State for start date
   const [endDate, setEndDate] = useState<Date | null>(null); // State for end date
+
+  // Image list for the slider
+  const images = [
+    "/assets/img/car1.png",
+    "/assets/img/car2.png",
+    "/assets/img/car3.png",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [opacity, setOpacity] = useState(0); // opacity for fade effect
+
+  // Change the image every 1 second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOpacity(0); // Start fading out the image
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setOpacity(1); // Fade in the next image
+      }, 300); // Wait for the fade out effect to finish
+    }, 6000); // Change every 1 second
+
+    return () => clearInterval(interval); // Clean up the interval on unmount
+  }, [images.length]);
+
+  // Manually change image on Next and Prev click
+  const nextImage = () => {
+    setOpacity(0); // Start fade out
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setOpacity(1); // Fade in the next image
+    }, 500); // Wait for the fade out effect to finish
+  };
+
+  const prevImage = () => {
+    setOpacity(0); // Start fade out
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+      setOpacity(1); // Fade in the previous image
+    }, 500); // Wait for the fade out effect to finish
+  };
 
   return (
     <>
@@ -21,13 +61,13 @@ export default function Welcome({
       <div className="text-black/50 dark:bg-black dark:text-white/50">
         <div className="relative flex items-center justify-center selection:bg-[#FF2D20] selection:text-white">
           <div className="relative w-full flex flex-col px-0 py-8">
-            <header className="flex px-[5%] flex-col md:flex-row">
-              <div className="flex justify-between items-center">
+            <header className="flex px-[5%] flex-col md:flex-row items-center justify-between">
+              <div className="flex justify-between items-center w-full">
                 <div className="flex justify-center items-center px-4">
                   <ApplicationLogo />
                 </div>
                 <div className="guest-access">
-                  <div className="flex justify-center items-center">
+                  <div className="flex justify-center items-center px-4">
                     <GuestNav />
                   </div>
                 </div>
@@ -44,7 +84,7 @@ export default function Welcome({
                   <>
                     <Link
                       href={route('login')}
-                      className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                      className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition text-nowrap hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                     >
                       Log in
                     </Link>
@@ -59,73 +99,115 @@ export default function Welcome({
               </nav>
             </header>
 
-            <main className="mt-6 flex-1">
-              <section className="w-full min-h-[50vh] m-auto bg-cover bg-center relative" style={{ backgroundImage: 'url("/assets/img/bg.png")', backgroundPosition: 'top' }}>
-                <div className="absolute inset-0 bg-black opacity-50 z-0"></div> {/* Overlay with opacity */}
+            <main className="mt-2 flex-1">
+              <section
+                className="w-full relative min-h-[50vh] bg-cover bg-center"
+                style={{ backgroundImage: 'url("/assets/img/bg.png")', backgroundPosition: 'top' }}
+              >
+                <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
                 <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full h-full px-4 py-8 relative z-10">
-                  <div className="flex-1 pl-24 max-w-md md:max-w-xl lg:max-w-4xl text-center md:text-left">
-                    <h1 className="text-3xl md:text-6xl font-semibold text-white mb-4">Your Perfect Ride Awaits!</h1>
-                    <h2 className="text-2xl md:text-4xl text-white mb-4">At Vehicle Rent Com, we make renting a car simple and stress-free.</h2>
-                    <h4 className="text-lg text-white mb-4">Choose from our wide selection of vehicles, and hit the road with confidence.</h4>
-                    <h4 className="text-lg text-white">No matter where you’re going, we’ve got the ride for you!</h4>
+                  <div className="flex-1 pl-4 md:pl-24 max-w-md md:max-w-xl lg:max-w-4xl text-center md:display-hidden z-10">
+                    <h1 className="text-3xl text-left md:text-6xl font-semibold text-white mb-4">
+                      Your Perfect Ride Awaits!
+                    </h1>
+                    <h2 className="text-lg text-left md:text-4xl text-white mb-4">
+                      At Vehicle Rent Com, we make renting a car simple and stress-free.
+                    </h2>
+                    <h4 className="text-lg text-left text-white mb-4">
+                      Choose from our wide selection of vehicles, and hit the road with confidence.
+                    </h4>
+                    <h4 className="text-lg text-left text-white">No matter where you’re going, we’ve got the ride for you!</h4>
                   </div>
 
                   <div className="flex-1 max-w-xs md:max-w-md lg:max-w-lg mt-8 md:mt-0">
-                    <img src="/assets/img/car6.png" alt="car" className="w-full h-auto object-contain" />
+                    <div className="relative">
+                      <div className="transition-opacity duration-500 ease-in-out opacity-0" style={{ opacity }}>
+                        <img
+                          src={images[currentIndex]}
+                          alt="car"
+                          className="w-full h-auto object-contain h-[340px] object-contain"
+                        />
+                      </div>
+                      <div className="absolute top-1/2 left-[-20px] transform -translate-y-1/2 z-10 text-white cursor-pointer">
+                        <FaChevronLeft onClick={prevImage} size={20} />
+                      </div>
+                      <div className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 text-white cursor-pointer">
+                        <FaChevronRight onClick={nextImage} size={20} />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </section>
 
-              <div className="w-full flex justify-center items-center py-8">
-                <div className="w-full max-w-md p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
-                  <h3 className="text-xl text-center mb-4 text-black dark:text-white">Select Booking Dates</h3>
-                  <div className="flex items-center justify-between mb-4">
-                    <div className="flex-1">
-                      <label className="block text-sm text-gray-600 dark:text-gray-200">Pick-up Date & Time</label>
-                      <DatePicker
-                        selected={startDate}
-                        onChange={(date: Date | null) => setStartDate(date)}
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        dateFormat="yyyy-MM-dd HH:mm"
-                        showTimeSelect
-                        timeFormat="HH:mm"
-                        timeIntervals={15}  // Sets the time intervals to 15 minutes
-                        className="w-full py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-blue-500"
-                        placeholderText="Select pick-up date and time"
-                      />
-                    </div>
+              <div className="absolute bottom-[-100px] left-0 right-0 px-[10%] py-16">
+              <div className="w-full flex gap-4 justify-around flex-wrap itmes-center bg-white shadow-xl rounded-lg p-8 lg:flex-nowrap z-50">
+                  <div className="pick-up mt-4">
+                    <label>Pick Up Location</label>
+                    <select className="w-full px-32 py-2 border border-gray-300 rounded-lg">
+                      <option>Location-1</option>
+                      <option>Location-2</option>
+                      <option>Location-3</option>
+                    </select>
+                  </div>
+                  <div className='flex flex-col justify-center items-center'>
+                    <div className="w-full max-w-md px-4 py-1 bg-white border-b-2 border-b-gray-300">
+                      <div className="flex items-center justify-between mb-4 space-x-4">
+                        <div className="flex-1">
+                          <label className="block text-sm text-gray-600">Pick-up Date & Time</label>
+                          <DatePicker
+                            selected={startDate}
+                            onChange={(date: Date | null) => setStartDate(date)}
+                            selectsStart
+                            startDate={startDate}
+                            endDate={endDate}
+                            dateFormat="yyyy-MM-dd HH:mm"
+                            showTimeSelect
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            className="w-full py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            placeholderText="Select pick-up date and time"
+                          />
+                        </div>
 
-                    <span className="mx-4 text-gray-500 dark:text-gray-300">to</span>
-
-                    <div className="flex-1">
-                      <label className="block text-sm text-gray-600 dark:text-gray-200">Drop-off Date & Time</label>
-                      <DatePicker
-                        selected={endDate}
-                        onChange={(date: Date | null) => setEndDate(date)}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        dateFormat="yyyy-MM-dd HH:mm"
-                        showTimeSelect
-                        timeFormat="HH:mm"
-                        timeIntervals={15}  // Sets the time intervals to 15 minutes
-                        className="w-full py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-blue-500"
-                        placeholderText="Select drop-off date and time"
-                      />
+                        <div className="flex-1">
+                          <label className="block text-sm text-gray-600">Drop-off Date & Time</label>
+                          <DatePicker
+                            selected={endDate}
+                            onChange={(date: Date | null) => setEndDate(date)}
+                            selectsEnd
+                            startDate={startDate}
+                            endDate={endDate}
+                            minDate={startDate}
+                            dateFormat="yyyy-MM-dd HH:mm"
+                            showTimeSelect
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            className="w-full py-2 px-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            placeholderText="Select drop-off date and time"
+                          />
+                        </div>
+                      </div>
                     </div>
+                    <div className='book-btn w-full mt-4'>
+                      <button className='btn w-full py-1 px-auto rounded-lg bg-gray-600 text-white border-[1px] border-gray-600 hover:text-black hover:bg-white'>Book Now</button>
+                    </div>
+                  </div>
+                  <div className="drop-off mt-4">
+                    <label>Drop Off Location</label>
+                    <select className="w-full px-32 py-2 border border-gray-300 rounded-lg">
+                      <option>Location-1</option>
+                      <option>Location-2</option>
+                      <option>Location-3</option>
+                    </select>
                   </div>
                 </div>
               </div>
             </main>
-
-            <footer className="py-16 text-center text-sm text-black dark:text-white/70">
-              Laravel v{laravelVersion} (PHP v{phpVersion})
-            </footer>
           </div>
         </div>
+        <footer className="py-16 text-center text-sm text-black dark:text-white/70">
+          Laravel v{laravelVersion} (PHP v{phpVersion})
+        </footer>
       </div>
     </>
   );
