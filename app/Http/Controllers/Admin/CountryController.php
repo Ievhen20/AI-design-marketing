9f66d98ef9ce4d245b429181a8d787725aeb61ca
Ievhen20 <Yevhen7856@ukr.net>
2025-02-13 16:10:37 -0800

    return to forntend-countries page

--- app/Http/Controllers/Admin/CountryController.php
+++ app/Http/Controllers/Admin/CountryController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Auth;
+use App\Models\Country;
+use Inertia\Inertia;
 
 class CountryController extends Controller
 {
     public function index() {
-        return "OK";
+        $countries = Country::all();
+        return Inertia::render('Admin/Company/Countries', [
+            'companies' => $companies,
+        ]);
     }
 }
